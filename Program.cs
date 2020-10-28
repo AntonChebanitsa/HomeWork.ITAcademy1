d546c7104f05c71315cf3e3319a8ab5ce3a3be00
AntonChebanitsa <67700374+AntonChebanitsa@users.noreply.github.com>
2020-10-28 19:36:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace HomeWork.ITAcademy1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double first;
+            double second;
+            Console.WriteLine("Enter two numbers: ");
+            Console.Write($"First: {first = Convert.ToDouble(Console.ReadLine())}");
+            Console.WriteLine(second = Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Summary is +\n" +
+                "Subtraction is -\n" +
+                "Mulplier is *\n" +
+                "Division is: /");
+
+            Console.Write("Еnter the required operation: ");
+            string op = Convert.ToString(Console.ReadLine());
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine($"Your answer is: {Addition(first, second)}");
+                    break;
+                case "*" :
+                    Console.WriteLine($"Your Answer is: {Multiplier(first,second)}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Your Answer is: {Subtraction(first,second)}");
+                    break;
+                case "/":
+                    Console.WriteLine($"Your Answer is: {Division(first,second)}");
+                    break;
+                default:
+                    break;
+            }
+            static double Addition(double first, double second)
+            {
+                return first + second;
+            }
+            static double Subtraction(double first, double second)
+            {
+                return first - second;
+            }
+            static double Multiplier(double first, double second)
+            {
+                return first * second;
+            }
+            static double Division(double first, double second)
+            {
+                try
+                {
+                    return first / second;
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Divide by zero error!!!");
+                    throw;
+                }
+            }
+
+        }
+
+    }
+}
