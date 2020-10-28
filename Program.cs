9de92db5b1378cdf94c2f04bb3f07d31c198151e
AntonChebanitsa <67700374+AntonChebanitsa@users.noreply.github.com>
2020-10-28 23:40:22 +0300

    Simplification of structures. Addition TODO

--- Program.cs
+++ Program.cs
@@ -7,61 +7,68 @@ namespace HomeWork.ITAcademy1
     {
         static void Main()
         {
-            double first;
-            double second;
+            double first = 0;
+            double second = 0;
             Console.WriteLine("Enter two numbers: ");
-            Console.WriteLine($"First: {first = Convert.ToDouble(Console.ReadLine())}");
-            Console.WriteLine($"Second: {second = Convert.ToDouble(Console.ReadLine())}\n\n" +
-                $"List of operations: \n");
-
+            try
+            {
+                Console.WriteLine($"First: {first = Convert.ToDouble(Console.ReadLine())}");
+                Console.WriteLine($"Second: {second = Convert.ToDouble(Console.ReadLine())}\n\n" +
+               $"List of operations: \n");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("This is not number!!\n");
+                Main();
+            }
+            // todo add more operations
             Console.WriteLine("Summary is +\n" +
                 "Subtraction is -\n" +
                 "Mulplier is *\n" +
                 "Division is: /");
+            //todo make a loop to validate user input
+            Console.Write($"Еnter the required operation: ");
 
-            Console.Write("Еnter the required operation: ");
-            string op = Convert.ToString(Console.ReadLine());
+            string flag = Convert.ToString(Console.ReadLine());
 
-            switch (op)
+            switch (flag)
             {
                 case "+":
-                    Console.WriteLine($"Your answer is: {Addition(first, second)}");
+                    Addition(first, second);
                     break;
-                case "*" :
-                    Console.WriteLine($"Your Answer is: {Multiplier(first,second)}");
+                case "*":
+                    Multiplier(first, second);
                     break;
                 case "-":
-                    Console.WriteLine($"Your Answer is: {Subtraction(first,second)}");
+                    Subtraction(first, second);
                     break;
                 case "/":
-                    Console.WriteLine($"Your Answer is: {Division(first,second)}");
+                    Division(first, second);
                     break;
                 default:
                     break;
             }
-            static double Addition(double first, double second)
+            static void Addition(double first, double second)
             {
-                return first + second;
+                Console.WriteLine($"Your answer is: {first + second}");
             }
-            static double Subtraction(double first, double second)
+            static void Subtraction(double first, double second)
             {
-                return first - second;
+                Console.WriteLine($"Your answer is: {first - second}");
             }
-            static double Multiplier(double first, double second)
+            static void Multiplier(double first, double second)
             {
-                return first * second;
+                Console.WriteLine($"Your answer is: {first * second}");
             }
-            static double Division(double first, double second)
+            static void Division(double first, double second)
             {
-                try
+                if (second == 0)
                 {
-                    return first / second;
+                    Console.WriteLine("Division by zero. Enter numbers again");
+                    Main();
                 }
-                catch (DivideByZeroException)
-                {
-                    Console.WriteLine("Divide by zero error!!!");
-                    throw;
-                }
+                else
+                    Console.WriteLine($"Your answer is: {first / second}");
             }
 
         }
