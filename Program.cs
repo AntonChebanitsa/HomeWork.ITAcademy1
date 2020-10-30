8d38e4990c01190651f208df624d4572c37d9b69
AntonChebanitsa <67700374+AntonChebanitsa@users.noreply.github.com>
2020-10-30 19:45:30 +0300

    Release?

--- Program.cs
+++ Program.cs
@@ -7,14 +7,15 @@ namespace HomeWork.ITAcademy1
     {
         static void Main()
         {
+            Console.WriteLine("Press any key to continue. If you want exit press ESC");
             Initialisations();
         }
 
         static void Initialisations()
         {
-            Console.WriteLine("Press any key to continue. If you want exit press ESC");
             while (Console.ReadKey(true).Key != ConsoleKey.Escape)
             {
+               
                 double first = 0;
                 double second = 0;
                 Console.WriteLine("Enter numbers: ");
@@ -39,7 +40,7 @@ namespace HomeWork.ITAcademy1
                 if (operation != "+" && operation != "-" && operation != "*" && operation != "/" && operation != "e" && operation != "!")
                 {
                     Console.WriteLine("Incorrect input!\nTry again from begin\n");
-                    continue ;
+                    Main() ;
                 }
                 if (operation != "!")
                 {
@@ -53,14 +54,14 @@ namespace HomeWork.ITAcademy1
                         Console.WriteLine("This is not number!!\n");
                         Main();
                     }
-                    InsertionFlag(first, second, operation);
+                    InsertionOperation(first, second, operation);
                 }
                 else
                     Console.WriteLine($"Factorial {first} is {Factorial(first)}");
             }
         }
 
-        static void InsertionFlag(double first, double second, string flag)
+        static void InsertionOperation(double first, double second, string flag)
         {
             bool IsIncorrectFlag = false;
             do
@@ -75,22 +76,20 @@ namespace HomeWork.ITAcademy1
                         Console.WriteLine($"Your answer is: {Multiplier(first, second)}\n");
                         break;
                     case "-":
-                        Console.WriteLine($"{first} subtract {second} is {Subtraction(first, second)}");
+                        Console.WriteLine($"{first} subtract {second} is: {Subtraction(first, second)}");
                         break;
                     case "/":
                         Console.WriteLine($"The result of division: {Division(first, second)}");
                         break;
                     case "e":
-                        Console.WriteLine($"Number {first} in extent {second} is:{Exponentiation(first, second)}\n");
+                        Console.WriteLine($"Number {first} in extent {second} is: {Exponentiation(first, second)}\n");
                         break;
                     default:
-                        Console.WriteLine("Invalid sign, try again");
                         IsIncorrectFlag = true;
                         break;
                 }
             }
             while (IsIncorrectFlag == true);
-
         }
 
         static double Addition(double first, double second)
